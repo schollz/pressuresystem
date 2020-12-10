c5c359c5cf5fc053c03529c250e8354ca682b610
Zack Scholl <zack.scholl@gmail.com>
2020-12-10 10:07:37 -0800

    add static

--- go.mod
+++ go.mod
@@ -6,5 +6,7 @@ require (
 	github.com/gin-gonic/gin v1.6.3
 	github.com/pkg/errors v0.9.1
 	github.com/schollz/logger v1.2.0
+	github.com/schollz/miti v0.6.0
 	github.com/tarm/serial v0.0.0-20180830185346-98f6abe2eb07
+	go.bug.st/serial v1.1.1
 )
--- main.go
+++ main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"bytes"
+	"net/http"
 	"os"
 	"os/signal"
 	"strings"
 	"sync"
 	"time"
 
-	"github.com/gin-gonic/gin"
-	"github.com/pkg/errors"
 	"github.com/schollz/logger"
+	"github.com/schollz/miti/src/log"
 	"github.com/tarm/serial"
 )
 
+var serialConfig *serial.Config
+var s *serial.Port
 var mu sync.Mutex
 
 func main() {
@@ -25,15 +27,6 @@ func main() {
 }
 
 func run() (err error) {
-	gin.SetMode(gin.ReleaseMode)
-	c := &serial.Config{Name: "COM5", Baud: 9600, ReadTimeout: time.Second * 1}
-	s, err := serial.OpenPort(c)
-	if err != nil {
-		err = errors.Wrap(err, "no com port")
-		return
-	}
-	s.Flush()
-	defer s.Close()
 
 	csig := make(chan os.Signal, 1)
 	signal.Notify(csig, os.Interrupt)
@@ -49,52 +42,115 @@ func run() (err error) {
 		}
 	}()
 
-	r := gin.Default()
-	r.StaticFile("/", "./index.html")
-	r.GET("/api", func(c *gin.Context) {
-		msg := c.DefaultQuery("msg", "")
-		if msg == "" {
-			c.JSON(200, gin.H{
-				"success": false,
-				"message": "no message",
-			})
-			return
-		}
-		err = write(s, msg)
-		if err != nil {
-			logger.Error(err)
-			c.JSON(200, gin.H{
-				"success": false,
-				"message": err.Error(),
-			})
-			return
-		}
-		if msg == "read" {
-			reply, err := read(s)
-			if err != nil {
-				logger.Error(err)
-				c.JSON(200, gin.H{
-					"success": false,
-					"message": err.Error(),
-				})
-				return
-			}
-			c.JSON(200, gin.H{
-				"success": true,
-				"message": strings.TrimSpace(reply),
-			})
-		} else {
-			c.JSON(200, gin.H{
-				"success": true,
-			})
-		}
-	})
-	logger.Infof("running on port 8080")
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 
-	r.Run() // listen and serve on 0.0.0.0:8080 (for windows "localhost:8080")
+	// r := gin.Default()
+	// r.StaticFile("/", "./index.html")
+	// r.GET("/api", func(c *gin.Context) {
+	// 	msg := c.DefaultQuery("msg", "")
+	// 	if msg == "" {
+	// 		c.JSON(200, gin.H{
+	// 			"success": false,
+	// 			"message": "no message",
+	// 		})
+	// 		return
+	// 	} else if msg == "open" {
+	// 		serialConfig = &serial.Config{Name: "COM5", Baud: 9600, ReadTimeout: time.Second * 1}
+	// 		s, err = serial.OpenPort(serialConfig)
+	// 		m := ""
+	// 		if err != nil {
+	// 			err = errors.Wrap(err, "no com port")
+	// 			m = err.Error()
+	// 		}
+	// 		s.Flush()
+	// 		c.JSON(200, gin.H{
+	// 			"success": err == nil,
+	// 			"message": m,
+	// 		})
+	// 		return
+	// 	} else if msg == "ports" {
+	// 		ports, err := stserial.GetPortsList()
+	// 		m := ""
+	// 		if err != nil {
+	// 			m = err.Error()
+	// 		} else {
+	// 			m = "found ports"
+	// 		}
+	// 		log.Debugf("ports: %+v", ports)
+	// 		c.JSON(200, gin.H{
+	// 			"success": err == nil,
+	// 			"message": m,
+	// 			"ports":   ports,
+	// 		})
+	// 		return
+	// 	}
+	// 	err = write(s, msg)
+	// 	if err != nil {
+	// 		logger.Error(err)
+	// 		c.JSON(200, gin.H{
+	// 			"success": false,
+	// 			"message": err.Error(),
+	// 		})
+	// 		return
+	// 	}
+	// 	if msg == "read" {
+	// 		reply, err := read(s)
+	// 		if err != nil {
+	// 			logger.Error(err)
+	// 			c.JSON(200, gin.H{
+	// 				"success": false,
+	// 				"message": err.Error(),
+	// 			})
+	// 			return
+	// 		}
+	// 		c.JSON(200, gin.H{
+	// 			"success": true,
+	// 			"message": strings.TrimSpace(reply),
+	// 		})
+	// 	} else {
+	// 		c.JSON(200, gin.H{
+	// 			"success": true,
+	// 		})
+	// 	}
+	// })
+	// logger.Infof("running on port 8080")
 
 	return
 }
+
+// handler is the main handler for all requests
+func handler(w http.ResponseWriter, r *http.Request) {
+	t := time.Now().UTC()
+	err := handle(w, r)
+	if err != nil {
+		log.Error(err)
+		w.Write([]byte(err.Error() + "\n"))
+	}
+	log.Infof("%v %v %v %s", r.RemoteAddr, r.Method, r.URL.Path, time.Since(t))
+}
+
+// handle is the function for the main routing logic of share.
+func handle(w http.ResponseWriter, r *http.Request) (err error) {
+	if strings.HasPrefix(r.URL.Path, "/open") {
+
+	} else {
+		page = "assets/" + strings.TrimPrefix(page, "/static/")
+		if strings.Contains(page, ".js") {
+			w.Header().Set("Content-Type", "text/javascript")
+		} else if strings.Contains(page, ".css") {
+			w.Header().Set("Content-Type", "text/css")
+		} else if strings.Contains(page, ".png") {
+			w.Header().Set("Content-Type", "image/png")
+		} else if strings.Contains(page, ".json") {
+			w.Header().Set("Content-Type", "application/json")
+		} else {
+			w.Header().Set("Content-Type", "text/html")
+		}
+		w.Write(b)
+	}
+}
+
 func write(s *serial.Port, data string) (err error) {
 	mu.Lock()
 	defer mu.Unlock()
